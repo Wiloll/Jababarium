7be1e7d31e39ebdfbe7ef28927d9be086c57f80f
kadi4ok <thunderrobot29@gmail.com>
2026-01-12 21:12:07 +0200

    commit

--- src/jababarium/MainJababarium.java
+++ src/jababarium/MainJababarium.java
@@ -1,17 +1,14 @@
-package example;
+package jababarium;
 
 import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
 import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class MainJababarium extends Mod{
 
-    public ExampleJavaMod(){
+    public MainJababarium(){
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
